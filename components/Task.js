12ba2ee2b53ebe92ad3192c806db26456dfde47c
Sahil <ss6611786@gmail.com>
2023-08-12 20:12:20 +0530

    create a basic layout and add some validations

--- components/Task.js
+++ components/Task.js
@@ -2,24 +2,43 @@ import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 
 const Task = () => {
-    const [input, setInput] = useState();
-    const [data, setData] = useState();
+    const [input, setInput] = useState('');
+    const [data, setData] = useState([]);
 
 
     const handleInput = () => {
-        setData(input);
+        if (!input) {
+            alert('provide a value first');
+            return;
+        }
+        setData((prev) => {
+            return [...prev, input]
+        });
+        setInput("");
+    }
+    const handleInputChange = (col) => {
+        setInput(col);
     }
     return (
         <View>
             <Text style={styles.heading}>Task Application</Text>
             <View style={styles.container}>
                 <TextInput style={styles.inputContainer} placeholder='task please...'
-                    onChange={(e) => { setInput(e.target.value) }}
+                    value={input}
+                    onChangeText={handleInputChange}
                 />
                 <Button title='add task' onPress={handleInput} />
             </View>
             <Text style={styles.subHeading}>User Tasks:</Text>
-
+            <View style={styles.tasksBox}>
+                {
+                    data?.map((val, index) => {
+                        return (
+                            <Text key={index} style={styles.list}>{val}</Text>
+                        )
+                    })
+                }
+            </View> 
         </View>
     )
 }
@@ -54,6 +73,20 @@ const styles = StyleSheet.create({
         textTransform: 'uppercase',
         borderWidth: 2,
         borderColor: 'green',
+    },
+    list: {
+        textAlign: 'center',
+        width: '20%',
+        borderWidth: 2,
+        borderColor: "orange",
+        margin: 5,
+        padding: 2,
+    },
+    tasksBox: {
+        marginLeft: 15,
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap-reverse',
     }
 })
-export default Task
+export default Task;
